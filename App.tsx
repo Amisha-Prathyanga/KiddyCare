c7dc444dbb3482b31e8f6ae87a83d06750cdf0c6
Amisha Prathyanga <spbamisha@gmail.com>
2024-10-27 09:12:46 +0530

    alert chages

--- App.tsx
+++ App.tsx
@@ -10,6 +10,9 @@ function App(): React.JSX.Element {
       .then(msg => {
         console.log('Meessge', msg);
       });
+    messaging().onMessage(res => {
+      console.log('m', res);
+    });
   }, []);
   return (
     <NavigationContainer>
